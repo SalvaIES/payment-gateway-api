933081f05c55c7c264554d3e5fe2b5b118723835
SalvaIES <s.martinezbolinche@edu.gva.es>
2026-05-10 13:34:30 +0200

    fix: DEFAULT_AUTO_FIELD y flake8 en requirements.txt

--- config/settings.py
+++ config/settings.py
@@ -134,5 +134,7 @@ REST_FRAMEWORK = {
     ],
 }
 
+# Salva
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY')
 STRIPE_WEBHOOK_SECRET = os.getenv('STRIPE_WEBHOOK_SECRET')
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
